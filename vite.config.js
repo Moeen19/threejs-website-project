46f2fd46e23c3707b308f789debe7dbed87a84bb
moeen <moeen12yt@gmail.com>
2024-07-22 13:36:12 +0500

    kek

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  root: "public",
+  root: 'public',
   build: {
-    outDir: "../dist",
+    outDir: '../dist',
     rollupOptions: {
       input: {
-        main: "./public/index.html",
-        about: "./public/about.html",
-        contact: "./public/contact.html",
-        more: "./public/more.html",
+        main: './public/index.html',
+        about: './public/about.html',
+        contact: './public/contact.html',
+        more: './public/more.html',
       },
     },
   },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/textures/**/*',
+          dest: 'textures'
+        }
+      ]
+    })
+  ]
 });
